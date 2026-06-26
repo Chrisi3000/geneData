3505e58e14e9eec21ea7daac2d2c97f4695b81a3
evaluftens <eva.luftensteiner@gmail.com>
2026-06-26 13:13:58 +0200

    irgendwas beim controller

--- Controllers/GeneDataItem.php
+++ Controllers/GeneDataItem.php
@@ -20,6 +20,49 @@ class Controllers_GeneDataItem extends Controllers_Base {
         $this->view->render($data);
     }
 
+    public function create()
+    {
+        $this->view->render(null);
+    }
+
+    public function post()
+    {
+        $obj = new Domains_GeneDataItem($_POST);
+        $data = $this->model->insert($obj);
+
+        http_response_code(302);
+        header('Location: /geneData/GeneDataItem/' . $data->id);
+        die();
+    }
+
+    public function put()
+    {
+        if (isset($this->params[0]) && !isset($GLOBALS["_PUT"]["id"])) {
+            $GLOBALS["_PUT"]["id"] = $this->params[0];
+        }
+
+        if (!isset($GLOBALS["_PUT"]["id"])) {
+            throw new Exception("Id not found");
+        }
+
+        $obj = new Domains_GeneDataItem($GLOBALS["_PUT"]);
+        $data = $this->model->update($obj);
+
+        http_response_code(201);
+        $this->view->render($data);
+    }
+
+//    public function delete()
+//    {
+//        if (!isset($this->params[0])) {
+//            throw new Exception("Id not found");
+//        }
+//
+//        $this->model->delete($this->params[0]);
+//
+//        http_response_code(204);
+//    }
+
     public function delete() {
         if(!Utils_Login::is_logged_in()){
             throw new Exceptions_Unauthorized("Unauthorized");
